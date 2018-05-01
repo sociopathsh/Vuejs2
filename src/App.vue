d0e8b92b8cfb458e3e176bfa89040912d864be48
Sherlock <sherlock@gmail.com>
2018-05-01 13:20:04 +0630

    slot part 2

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
     <form-helper>
-      <h1 slot="title">I am the title slot</h1>
-      <p slot="text">I am the paragraph text for the slot</p>
+      <div slot="form-header">
+        <h3>This is the title of a form</h3>
+        <p>This is some info about the form</p>
+      </div>
+      <div slot="form-fields">
+        <input type="text" placeholder="name" required />
+        <input type="password" placeholder="password" required />
+      </div>
+      <div slot="form-controls">
+        <button v-on:click="handleSubmit">Submit</button>
+      </div>
     </form-helper>
   </div>
 </template>
